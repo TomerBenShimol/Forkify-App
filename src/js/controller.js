4f3fed2e4e193bbb4b8b06e8d0a25b99709b1c89
Tomer Ben Shimol <tomerbenshimol3164@gmail.com>
2023-04-17 12:42:37 +0300

    Loading a recipe from API
    https://forkify-api.herokuapp.com/v2

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,36 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+const apiKey = '30dd6420-110d-4485-81ee-19065dd835d0';
+
+const getRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    return recipe;
+  } catch (err) {
+    alert(err);
+  }
+};
+
+(async function () {
+  const recipe1 = await getRecipe('5ed6604591c37cdc054bc886');
+  console.log(recipe1);
+})();
 
 ///////////////////////////////////////
